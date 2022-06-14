1f43d6d80292cd4ed81150f40619e1a64ff73d22
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-06-14 20:27:08 -0300

    backup home

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,13 +2,16 @@
 
 function BinarioADecimal(num) {
   let split = num.split('').reverse();
-  let j = 0;
+  //let j = 0;
   let sum = 0;
   for (let i = 0 ; i < split.length ; i++) {
-    sum += (parseInt(split[i], 10) * Math.pow(2, j));
-    j++;
+    sum += (parseInt(split[i], 10) * Math.pow(2, i));
+    //j++;
   }
   return sum;
+  //let qwe = parseInt(num, 2);
+  //return qwe;
+  
 }
 
 function DecimalABinario(num) {
@@ -19,6 +22,8 @@ function DecimalABinario(num) {
       result = Math.floor(result / 2);
     } while (result !== 0);
   return binary.join('');
+  //let asd = num.toString(2);
+  //return asd;
 }
 
 module.exports = {
