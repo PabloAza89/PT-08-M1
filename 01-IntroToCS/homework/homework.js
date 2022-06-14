98c273abe007bc1e842f410a7f8ed59a4ad6ed04
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-06-14 01:43:40 -0300

    Backup Home

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,25 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // asd
-
+  let split = num.split('').reverse();
+  let j = 0;
+  let sum = 0;
+  for (let i = 0 ; i < split.length ; i++) {
+    sum += (parseInt(split[i], 10) * Math.pow(2, j));
+    j++;
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let binary = [];
+    let result = num;
+    do {
+        binary.unshift(result % 2);
+        result = Math.floor(result / 2);
+        
+    } while (result !== 0);
+    return binary.join('');
 }
 
 
