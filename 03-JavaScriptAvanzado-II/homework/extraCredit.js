31fd63ae5b192d1fce8e50bfd8b4f4170e31165a
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-06-23 00:31:07 -0300

    backup home

--- 03-JavaScriptAvanzado-II/homework/extraCredit.js
+++ 03-JavaScriptAvanzado-II/homework/extraCredit.js
@@ -1,12 +1,10 @@
-//### Repeatify
-
-//Crear un método `repeatify` que este disponible para _todos_ los objetos `Strings`. 
-//Esta función debe aceptar un `entero` que indica cuantas veces el string tiene que repetirse. 
-//La función retorna el string repetido el número de veces que indicamos. Controlar que el número no sea menor que cero, y si es cero que devuelva `''` (String vacío).
-
-//```javascript
-//console.log('hola'.repeatify(3));   //holaholahola
-//```
+// ### Repeatify
+//
+// Crear un método `repeatify` que este disponible para _todos_ los objetos `Strings`. 
+// Esta función debe aceptar un `entero` que indica cuantas veces el string tiene que repetirse. 
+// La función retorna el string repetido el número de veces que indicamos. Controlar que el número no sea menor que cero, y si es cero que devuelva `''` (String vacío).
+//
+// console.log('hola'.repeatify(3));   //holaholahola
 
 String.prototype.repeatify = function(number) {
     let string = '';
@@ -20,3 +18,36 @@ String.prototype.repeatify = function(number) {
 }
 
 console.log('hola'.repeatify(3));
+
+// ### Shapes
+//
+// * Crea un objeto llamado `shape` que tenga una propiedad `type` y un método `getType`.
+// * Ahora defini una función `Triangle` cuyo prototipo sea `shape`. Los objetos creados con `Triangle` deberían tener tres propiedades: `a`, `b` y `c`. 
+//   Que representan cada lado del triángulo. `type` debe ser `Triangle`.
+// * Agregá un nuevo método al prototipo llamado `getPerimeter`.
+
+let shape = {
+    type: 'Triangle',
+    getType: function() {
+        return this.type;
+    }
+}
+
+function Triangle(a,b,c) {
+    a = a;
+    b = b;
+    c = c;
+}
+
+Triangle.prototype.shape = function() {}
+
+// Probá tu solución con el siguiente código:
+var t = new Triangle(1, 2, 3);
+console.log(t instanceof Triangle)
+// true
+console.log(shape.prototype.isPrototypeOf(t));
+// true
+console.log(t.getPerimeter());
+// 6
+console.log(t.getType());
+// "Triangle"
