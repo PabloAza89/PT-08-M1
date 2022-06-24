d4f3a5a31e9332468d2a6c5e10bddce0233637be
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-06-24 18:02:59 -0300

    backup home

--- EXTRA-Testing/demo/sum.js
+++ EXTRA-Testing/demo/sum.js
@@ -1,4 +1,6 @@
 function sum(a, b) {
+  if (typeof a !== 'number') throw new TypeError('First parameter is not a number.');
+  if (typeof b !== 'number') throw new TypeError('Second parameter is not a number.');
   return a + b;
 }
 
--- EXTRA-Testing/homework/homework.js
+++ EXTRA-Testing/homework/homework.js
@@ -1,42 +1,40 @@
+const layout = [ // LETTER/ROW v // NUMBER >
+    [{type: 'VIP', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}],
+    [{type: 'NORMAL', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}, {type: 'NORMAL', booked: false}],
+    [{type: 'NORMAL', booked: false}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: false}],
+    [{type: 'ECONOMIC', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'ECONOMIC', booked: false}],
+    [{type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: true}, {type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: false}]
+  ];
+
+function getRowNumber(letter) {
+    return letter.charCodeAt(0) - 65; // A --> 0
+}
+
 function getSeat(letter, number) {
-    const layout = [
-        [{type: 'VIP', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}],
-        [{type: 'NORMAL', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}, {type: 'NORMAL', booked: false}],
-        [{type: 'NORMAL', booked: false}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: false}],
-        [{type: 'ECONOMIC', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'ECONOMIC', booked: false}],
-        [{type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: true}, {type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: false}]
-      ];
-      
-    const numberRow = getRowNumber(letter);
-    const layoutRows = layout[numberRow];
-    const seat = layoutRows[number];
-    return seat;
+    const numberRow = getRowNumber(letter); // A1 // A --> 0 
+    const layoutRows = layout[numberRow]; // layout[0]
+    const seat = layoutRows[number]; // layout[0][1] 
+    return seat; // true
 } 
-//console.log(getSeat('C', 0))
-function checkSeatStatus(row, number) {
+
+function checkSeatStatus(row, number) { // A1 // ROW === LETTER
     if (typeof row !== 'string') {
         throw new TypeError('First parameter is not a string');
     }    
     if (typeof number !== 'number') {
         throw new TypeError('Second parameter is not a number');
-    }    
-
-    const seat = getSeat(row, number);
-    return seat.booked;
+    }
+    const seat = getSeat(row, number); // A1
+    return seat.booked; // TRUE // layout[0][1].booked
 }
 
-function getRowNumber(letter) {
-    return letter.charCodeAt(0) - 65;
-}
-
-function book(row, number) {
-    if (checkSeatStatus(row,number)) {
+function book(row, number) { // A1 // E3
+    if (checkSeatStatus(row,number)) { // A1(TRUE)
         return `Seat in ${row}${number} is already booked`;
     }    
-    const seat = getSeat(row, number);
-    seat.booked = true;
-
-    return `Seat in ${row}${number} successfully booked`;
+    const seat = getSeat(row, number); // E3 --> 4[3] (false)
+    seat.booked = true; // layout[4][3] = true; (false --> true)
+    return `Seat in ${row}${number} successfully booked`; // assigned
 }  
 
 module.exports = {
