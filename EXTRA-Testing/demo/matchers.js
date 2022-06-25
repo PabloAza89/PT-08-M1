683d923aad32a23bf02ce3bf5da1d63854ace1d5
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-06-25 16:21:15 -0300

    backup home

---
+++ EXTRA-Testing/demo/matchers.js
@@ -0,0 +1,27 @@
+function checkNumber(number) {
+    
+    return number;
+}
+console.log(checkNumber(3));
+const string = 'Franco';
+const obj = {name: 'Franco', age: 27};
+const array = [1,2,3,4,5];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    checkNumber,
+}
--- EXTRA-Testing/demo/matchers.test.js
+++ EXTRA-Testing/demo/matchers.test.js
@@ -1,45 +1,40 @@
-xdescribe('Tests that will pass', () => {
+const {
+  checkNumber,
+} = require('./matchers');
+
+describe.only('Tests that will pass', () => {
   it('should pass if exact match', () => {
-    const number = 3;
-    const string = 'Franco';
-    expect(number).toBe(3);
+    expect(checkNumber(3)).toBe(3);
     expect(string).toBe('Franco');
   });
   it('should pass if recursively match (object)', () => {
-    const obj = {name: 'Franco', age: 27};
     expect(obj).toEqual({name: 'Franco', age: 27});
   });
-  
   it('should pass if recursively match (array)', () => {
-    const array = [1,2,3,4,5];
     expect(array).toEqual([1,2,3,4,5]);
   });
 })
 
-xdescribe('Tests that will not pass', () => {
+describe('Tests that will not pass', () => {
   it('should fail if not exact match (object)', () => {
     const obj = {name: 'Franco', age: 27};
     expect(obj).toBe({name: 'Franco', age: 27});
   });
-  
   it('should fail if not exact match (array)', () => {
     const array = [1,2,3,4,5];
     expect(array).toBe([1,2,3,4,5]);
   });
 })
 
-xdescribe('Extra matchers', () => {
-
+describe('Extra matchers', () => {
     it('toBeNull', () => {
       expect(null).toBeNull();
       // expect(undefined).toBeNull();
     });
-  
     it('toBeUndefined', () => {
       expect(undefined).toBeUndefined();
       // expect(null).toBeUndefined();
     });
-
     it('toBeDefined', () => {
       expect(2).toBeDefined();
       expect(0).toBeDefined();
@@ -52,7 +47,6 @@ xdescribe('Extra matchers', () => {
       expect(null).toBeDefined();
       // expect(undefined).toBeDefined();
     });
-
     // toBeFalsy --> opposite
     it('toBeTruthy', () => {
       expect(true).toBeTruthy();
@@ -67,7 +61,6 @@ xdescribe('Extra matchers', () => {
       // expect(undefined).toBeTruthy();
       // expect(null).toBeTruthy();
     });
-
     it('toBeCloseTo', () => {
       // expect(0.1 + 0.2).toEqual(0.3);
       expect(0.1 + 0.2).toBeCloseTo(0.3);
@@ -75,27 +68,20 @@ xdescribe('Extra matchers', () => {
       expect(0.11).toBeCloseTo(0.1, 1);
       expect(0.174927142).toBeCloseTo(0.17492, 4);
     });
-
     it('toMatch', () => {
       expect('Un texto largo que tiene Franco dentro de sus palabras').toMatch('Franco');
       expect('Un texto largo que tiene Franco dentro de sus palabras').toMatch(/Franco/);
       expect('Un texto largo que tiene Franco dentro de sus palabras').toMatch(/palabras$/);
     });
-
     it('toContain', () => {
       const array = ['Franco', 'Toni', 'Martu', 'Mati'];
       expect(array).toContain('Martu');
       expect(array).not.toContain('Diego');
-
       expect('Soy Henry').toContain('Henry');
     });
-
     // also used as .toThrowError
     it('toThrow', () => {
-      function error() {
-        throw new TypeError('An error');
-      }
-
+      function error() {throw new TypeError('An error');}
       // function definition, not executed!
       expect(error).toThrow();
       expect(error).toThrow('An error');
@@ -107,7 +93,3 @@ xdescribe('Extra matchers', () => {
       expect(error).toThrow(new TypeError('An error'));
     });
 });
-
-
-
-
