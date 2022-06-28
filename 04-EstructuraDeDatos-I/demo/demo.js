7bde962a151bef06a930a5f7fb1494cbf4497349
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-06-28 11:48:15 -0300

    backup work

--- 04-EstructuraDeDatos-I/demo/demo.js
+++ 04-EstructuraDeDatos-I/demo/demo.js
@@ -3,20 +3,18 @@
 function countDown(number) {
     // imprimo el numero
     console.log(number);
-  
     // decrementar el numero
     const newNumber = number - 1;
-  
     // caso base o caso de corte
     if (newNumber > 0) {
       countDown(newNumber); //
     }
-  }
+}
   
-  //countDown(4);
+//countDown(4);
   
-  // Ejemplo 2
-  let parImpar = (numero) => {
+// Ejemplo 2
+let parImpar = (numero) => {
     //10
     if (numero < 0) {
       throw new Error('No se admiten numeros negativos');
@@ -27,52 +25,53 @@ function countDown(number) {
     } else {
       return parImpar(numero - 2);
     }
-  };
-  //console.log(parImpar(-3)); // error
-  // console.log(parImpar(20)); // Par
-  // console.log(parImpar(75)); // Impar
-  // console.log(parImpar(98)); // Par
-  // console.log(parImpar(113)); // Impar
+};
+
+//console.log(parImpar(-3)); // error
+// console.log(parImpar(20)); // Par
+// console.log(parImpar(75)); // Impar
+// console.log(parImpar(98)); // Par
+// console.log(parImpar(113)); // Impar
   
-  /* 
-  Recursion:
-  funcion que se llama a si misma => hasta que algo (caso de corte/ base) para la ejecucion y empieza a resolver
+/* 
+Recursion:
+funcion que se llama a si misma => hasta que algo (caso de corte/ base) para la ejecucion y empieza a resolver
   
-  4! = 4x3!
-      3! = 3x2!
-           2! = 2x1!
-             1! = 1
-  */
-  // 5! = 5 * 4 * 3 * 2 * 1 = 120
-  // 4! = 4 * 3 * 2 * 1 = 24
-  // 3! = 3 * 2 * 1 = 6
+4! = 4x3!
+    3! = 3x2!
+        2! = 2x1!
+            1! = 1
+*/
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+// 4! = 4 * 3 * 2 * 1 = 24
+// 3! = 3 * 2 * 1 = 6
   
-  //----- Extra Homework ----------------
-  // a) Volver a hacer las funciones de binario a decimal (y viceversa) pero usando recursion
-  // b) escribe una funcion que reciba un string y lo devuelva dado vuelta ej: Hello => olleH
-  // c) escribe una funcion que reciba dos strings (una frase/palabra y una letra) y que devuelva
-  // la cantidad de veces que se repite esa letra. ej: 'Javascript', 'a' => 2
+//----- Extra Homework ----------------
+// a) Volver a hacer las funciones de binario a decimal (y viceversa) pero usando recursion
+// b) escribe una funcion que reciba un string y lo devuelva dado vuelta ej: Hello => olleH
+// c) escribe una funcion que reciba dos strings (una frase/palabra y una letra) y que devuelva
+// la cantidad de veces que se repite esa letra. ej: 'Javascript', 'a' => 2
   
-  //-------------------- SET-------------
+//-------------------- SET-------------
   
-  let a = new Set();
-  a.add(7);
-  a.add(8);
-  a.add(7);
-  console.log(a);
-  a.delete(8);
-  console.log(a);
+let a = new Set();
+a.add(7);
+a.add(8);
+a.add(7);
+console.log(a);
+a.delete(8);
+console.log(a);
   
-  let b = [8, 7, 5, 22, 1, 1, 1, 2, 3, 5, 6, 2, 7, 3, 7, 1, { hola: 'hola' }];
-  [...b] = new Set(b);
-  console.log(b);
+let b = [8, 7, 5, 22, 1, 1, 1, 2, 3, 5, 6, 2, 7, 3, 7, 1, { hola: 'hola' }];
+[...b] = new Set(b);
+console.log(b);
   
-  // STACK  => Last in, First Out
+// STACK  => Last in, First Out
   
-  function Stack() {
-    // x detras es un array
-  }
+function Stack() {
+	// x detras es un array
+}
   
-  Stack.prototype.add = function () {
-    // agrega un elemento al final
-  };
+Stack.prototype.add = function () {
+	// agrega un elemento al final
+};
