89ad4b8ac01f47cbcf5b2509ad94c8812ba3fb27
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-07-05 01:18:13 -0300

    h

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,72 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+// 1) funcion contructora
+// 2) metodo insert => insertar un sub-arbol
+//       tengo un valor?
+//          sos menor?
+//               recursion ...
+//               creo un nuevo arbol
+//           sos mayor?
+//               recursion ...
+//               creo un nuevo arbol
+// 3) metodo contains => true or false
+//        recorro con recursion
+//        sos el valor?
+//           recursion
+//              derecha...
+//           recursion
+//              izquierda...
+//       retorno false xq no lo encontre
+// 4) size => cantidad de nodos (subarboles contando la raiz)
+//        contador pero con recursion
+// 5) depth-first => recibe dos argumentos (cb, order)
+//      in order
+//      pre order
+//      post order
+// 6) recorrido bfs
+
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
+  this.prev = null;  
+}
+
+BinarySearchTree.prototype.insert = function(number) {
+  if (this.left === null && this.right === null && this.prev === null) {
+    let tree = new BinarySearchTree(number);
+    return tree;
+  }    
+}
+
+BinarySearchTree.prototype.size = function() {
+  let set = new Set();
+  if (!set.has(this.value)) {
+    set.add(this.value);
+  } else {
+    if (this.left === null && this.right === null) {
+      return set.size();
+    }
+  }
+  if (this.left.value) {
+    set.add(this.left)
+  }
+}
+
+BinarySearchTree.prototype.contains = function() {
+  
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function() {
+ 
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function() {
+  
+}
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
