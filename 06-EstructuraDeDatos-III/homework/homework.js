0953945d49596f31c4ae0fcaff6e433e14e613ea
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-07-05 18:02:27 -0300

    w

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,68 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
+}
+
+BinarySearchTree.prototype.insert = function(newNum) {
+  let node = new BinarySearchTree(newNum);
+  if (newNum > this.value) {
+    if (!this.right) {
+      this.right = node;
+    } else {
+      this.right.insert(newNum);
+    }
+  } else {
+    if (newNum < this.value) {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.insert(newNum);
+      }
+    }
+
+    
+  }
+  return 'Successfully inserted';
+}
+
+BinarySearchTree.prototype.size = function() {
+  let nodeRight = this.right;
+  let nodeLeft = this.left;
+  let currentVal = this.value;
+  let set = new Set();
+  if (!nodeRight && !nodeLeft && !set.has(currentVal)) {
+    set.add(currentVal)
+    return set.size;
+  }
+  while (nodeRight.right && !set.has(nodeRight.value)) {
+    nodeRight = nodeRight.right;
+  }
+}
+
+let qq = new BinarySearchTree(20)
+console.log(qq.size())
+console.log(qq)
+console.log(qq.insert(15))
+console.log(qq.insert(25))
+console.log(qq.insert(10))
+console.log(qq)
+
+
+BinarySearchTree.prototype.contains = function() {
+
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function() {
+
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function() {
+
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
