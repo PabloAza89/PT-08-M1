2a0b4628a7aff073d5f8a48c66ab2d4d0d97c87f
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-07-08 11:50:01 -0300

    w

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,7 @@
 'use strict'
+
+const { arrayReplaceAt } = require("markdown-it/lib/common/utils");
+
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -54,15 +57,40 @@ function bubbleSort(array) {
 //console.log(bubbleSort([5, 1, 4, 2, 8]))
 //console.log(bubbleSort([11, 0, 3, 1, 7, 12, 4, 6, 24, 2, 8, 5, 9]))
 
+  // [5, 1, 4, 2, 8] => [1, 2, 4, 5, 8]
+  // [5, 1, 4, 2, 8]
+  // [1, 4, 2, 5, 8]
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0 ; i < array.length ; i++) {
+    if (array[i + 1] < array[i]) { // i = 0
+      let toMove = array[i + 1]
+      array.splice(i + 1, 1)
+      //console.log(i) // 0
+      //console.log(toMove) // 1
+      //console.log(array) // [5, 4, 2, 8]
+      for (let j = i; j >= 0 ; j--) {
+        //console.log(array[j-1])
+        if (toMove < array[j] && array[j -1] === undefined || toMove < array[j] && array[j - 1] < toMove)  {
+          array.splice(j, 0, toMove)
+        }
+      }
+    }
+  }
+  return array;
 }
 
+console.log(insertionSort([5, 1, 4, 2, 8]))
+//                        [5, 4, 2, 8]    // i = 0
+//                        [1, 5, 4, 2, 8] // i = 0
+//                        [1, 5, 2, 8]    // i = 1
+//                        [1, 4, 5, 2, 8] // i = 1
+//                        [1, 4, 5, 2, 8] // i = 1
+//
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
