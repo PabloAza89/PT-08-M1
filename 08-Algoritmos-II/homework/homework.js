62f6f670adf28f255bc2a14134ebf982eba71305
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-07-09 01:33:43 -0300

    h

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -5,16 +5,14 @@
 // [10, 10, 16, 12] => [10, 10, 12, 16]
 // [10, -2, -7, 4] => [-7, -2, 4, 10]
 
-function quickSort(array) {
+function quickSort(array, left = [], equal = [], right = []) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
   // [5, 1, 4, 2, 8] => [1, 2, 4, 5, 8]
-  let left = [];
-  let equal = [];
-  let right = [];
-  let pointer = array[Math.floor((Math.random() * array.length) * 1)]
+  
+  let pointer = array[Math.floor((Math.random() * array.length) * 1)];
   for (let i = 0 ; i < array.length ; i++) {
     if (array[i] < pointer) {
       left.push(array[i])           
@@ -23,12 +21,20 @@ function quickSort(array) {
       equal.push(array[i])           
     }
     if (array[i] > pointer) {
-      left.push(array[i])           
+      right.push(array[i])           
     } 
-    
-    //return quickSort(left.concat(equal).concat(right))
-    return pointer
+    // console.log(left)
+    // console.log(equal)
+    // console.log(right)
+    // console.log(pointer)
   }
+   if (left.lenght === 1 || left[0] === undefined) right.quickSort(array = right)
+   if (right.lenght === 1 || right[0] === undefined) left.quickSort(array = left)
+   if ((left.lenght === 1 || left[0] === undefined) && (right.lenght === 1 || right[0] === undefined)) left.concat(equal).concat(right)
+
+   
+  
+
 
   //console.log(pointer)
 
