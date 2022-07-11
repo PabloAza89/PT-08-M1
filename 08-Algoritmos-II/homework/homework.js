9869a2c495927d6eca945b411c1b26d6e3ed789e
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-07-10 23:03:29 -0300

    h

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -34,13 +34,61 @@ function quickSort(array, left = [], equal = [], right = []) {
 //console.log(quickSort([5, 1, 4, 2, 8]))
 //console.log(quickSort([5, 1, 4]))
 
-function mergeSort(array) {
-  // Implementar el método conocido como mergeSort para ordenar de menor a mayor
-  // el array recibido como parámetro
-  // Devolver el array ordenado resultante
-  // Tu código:
+// function mergeSort(array, left = [], right = [], result = []) {
+//   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
+//   // el array recibido como parámetro
+//   // Devolver el array ordenado resultante
+//   // Tu código:
+//   let pointer = Math.floor((array.length - 1) / 2);
+  
+
+  
+//   for (let i = 0 ; i < array.length ; i++) {
+//     if (i <= pointer) {
+//       left.push(array[i])
+//     } else {
+//       right.push(array[i])
+//     }
+//   }
+  
+//   if (left.length > 1) mergeSort(left, result) 
+//   if (right.length > 1) mergeSort(right, result)
+
+//   for (let i = 0; i < left.length ; i++) {
+//     if (left)
+//   }
+  
+//   console.log(left)
+//   console.log(right)
+//   return result;
+// }
+
+// console.log(mergeSort([5, 1, 4, 2, 8]))
+
+function prueba() {
+  //let pointer = Math.floor((array.length - 1) / 2);
+  let left = [2, 4, 9];
+  let right = [1, 5, 8];
+  let result = [];
+  
+  let i = 0;
+  let j = 0;
+ 
+  while (i < left.length || j < right.length) {
+    if (left[i] < right[j] || right[j] === undefined) {
+      result.push(left[i])
+      i++      
+    } 
+    else {
+      result.push(right[j])
+      j++
+    }
+  }
+  return result
 
 }
+
+console.log(prueba());
 
 // No modificar nada debajo de esta línea
 // --------------------------------
